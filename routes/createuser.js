75edf05bde15b1a5af05e0da9bfcd15784ac9077
dcheris <97857929+dcheris@users.noreply.github.com>
2022-11-18 01:47:47 -0600

    adding application logic

--- routes/createuser.js
+++ routes/createuser.js
@@ -7,3 +7,6 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+//the statement below contains a render function - the first argument is the view name and the second argument is an object with one key/value pair.
+res.render('createuser', { title: 'Create Account'})
